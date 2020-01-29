a732df914cbd524abe3b8762c67d8a439d65c317
Jeffrey Wubbenhorst <Moogert@users.noreply.github.com>
2020-01-29 17:10:59 -0500

    wow i did a lot since my last commit
    ok so basically the encoder works and i think i have PWM working fine

--- Arduino Code/yungWubbzProduction/yungWubbzProduction.ino
+++ Arduino Code/yungWubbzProduction/yungWubbzProduction.ino
@@ -16,8 +16,11 @@ I need to do this with PWM.
 
 ## PINZ:
 * 2,3: encoder pins
-*
-
+* 12: motor, for now
+* A6: read status. It would be cool to do some kind of weird
+      analog multiplexing thing here to only use one pin for
+      like 5 diff functions
+* LED out: FAN pin = 14??
 
 */
 
@@ -27,16 +30,15 @@ I need to do this with PWM.
 bool has_display = true; //is there a display or not
 volatile bool show_speed;
 // int speed_pin = 3; // throttle
-int power_pin = 7;
-int motor_relay_pin = 18; // #FLAG
-// int esc_pin = 10; // ESC = electronic speed control
+int power_pin = 12; // powers motor via PWM
 int led_pin = 17; // #FLAG probably change this to A7
-
+int brake_pin = A6;
+int brake_light_pin = 14; // ?
 
 // ## PWM STUFF {
- TCCR1B = TCCR1B & 0b11111001; // something about a 32khz pwm?? idk
+// TCCR1B = TCCR1B & 0b11111001; // something about a 32khz pwm?? idk
 
- // } ## PWM STUFF
+// } ## PWM STUFF
 
 
 // ## ENCODER STUFF {
@@ -46,20 +48,21 @@ volatile long position  = -999;
 // } ## ENCODER STUFF
 
 
- // don't know what this does  {
+// don't know what this does  {
 uint8_t off_level = 0;
 uint8_t on_min_level = 96;
 volatile unsigned long last_time;
 uint16_t circumference = 200; //circumference of the wheel in mm
 uint16_t damping_factor = 2; // filter parameter
-volatile double speed; // #FLAG no idea what the units for this are
+volatile double speed = 0; // #FLAG no idea what the units for this are
 volatile unsigned long prev_show_time;
 volatile bool motor_state;
 volatile double motor_speed;
+volatile int pwm = 0;
+volatile bool brake = true; // starting off with the brake ON
 double max_speed = 10; // what is this in
 volatile bool button_ok = true;
 // } // dont knpw what this does
-
 
 void display_speed() {
   if (has_display) {
@@ -69,33 +72,46 @@ void display_speed() {
 }
 
 void update_speed() { // looks like there's a 'tick' that happens every
-                      // time the wheel goes round. lasers, ?
-  unsigned long new_time = millis();
-  unsigned long diff = new_time - last_time;
-  double new_speed = circumference/diff; //speed in mm per millisecond
-  speed += (new_speed - speed) / damping_factor; //LPF damping
-  last_time = new_time; // pretty cool, got a filter
-  Serial.println("update_speed");
-  if (show_speed) {
-    display_speed();
-  }
+// time the wheel goes round. lasers, ?
+unsigned long new_time = millis();
+unsigned long diff = new_time - last_time;
+double new_speed = circumference/diff; //speed in mm per millisecond
+speed += (new_speed - speed) / damping_factor; //LPF damping
+last_time = new_time; // pretty cool, got a filter
+Serial.println("update_speed");
+if (show_speed) {
+  display_speed();
+}
 }
 
-int get_esc_out() { // this likely converts to a speed controller
-  uint16_t outval = map((int) motor_speed, 0, (int) max_speed, 127, 130); // why 127 -> 130
-  return outval;
+void lightDance()
+{ // this is where the fun LED updating happens
+// if brake is on, turn brake light on
+if(brake){ digitalWrite(brake_light_pin, HIGH);} // crank that
+// we'll get turn signals and such sorted out a little later
+
 }
 
-void power_motor() {
-  digitalWrite(led_pin, HIGH);
-  motor_state = true;
+void convertSpeed()
+{
+  // fuerte
+  // read encoder val
+  if(position<=0){ position = 0;}
+  if(position>400){ position = 400;} // assuming 400 pulses / revolution
+
+  pwm = map(position, 0, 400, 0,255);
+  Serial.println(pwm);
+}
+
+void power_motor()
+{
+  convertSpeed();
   motor_speed = min(speed, max_speed);
   //turn relay switch to motor
-  digitalWrite(motor_relay_pin, HIGH);
+  // digitalWrite(power_pin, HIGH); // maybe power a running light here?
   Serial.println("More power!!");
-  delay(100);
-  /* analogWrite(esc_pin, 130);  // honey we don't have an ESC
-  */
+  // delay(100); // need to figure out timing
+  analogWrite(power_pin, motor_speed);
 }
 
 
@@ -107,101 +123,69 @@ void stop_motor() {
   //turn relay switch off.
   //I think this should be done immediately to start regen -> doesn't really
   // work for li-ion, route energy elsewhere plz (maybe to capacitor for lights?)
-  digitalWrite(motor_relay_pin, LOW);
-  analogWrite(esc_pin, 100);
 }
 
-
-void test_fall() { // doubt imma need this
-  if (button_ok) {
-    button_ok = false;
-    Serial.print("test_fall triggered...");
-    analogWrite(esc_pin, 90);
-    Serial.print("writing analog esc value...");
-    Serial.println(millis());
-  }
-}
-
-void test_rise() { // ...or this
-  Serial.println("test rise");
-  if (!button_ok) {
-    Serial.print("test_rise triggered...");
-    button_ok = true;
-    Serial.println(millis());
-  }
-}
-
-
-void test_change() {
-  Serial.println("test change");
-  if (digitalRead(power_pin)) {
-    if (!button_ok) {
-      Serial.print("test_rise triggered...");
-      analogWrite(esc_pin, 90);
-      button_ok = true;
-      Serial.println(millis());
-    }
-  } else {
-    if (button_ok) {
-      button_ok = false;
-      Serial.print("test_fall triggered...");
-      analogWrite(esc_pin, 96);
-      Serial.print("writing analog esc value...");
-      Serial.println(millis());
-    }
-  }
-}
 
 void setup()
 {
-  // pinMode(speed_pin, INPUT); // getting rid of this bc of encoder
-  pinMode(power_pin, INPUT); // guessing this tells us if there's power??
-  pinMode(motor_relay_pin, OUTPUT); // #FLAG: is this PWM
-  // pinMode(esc_pin, OUTPUT); // still dont know what this does
+  cli(); // stop interrupt
+  pinMode(power_pin, OUTPUT);
   pinMode(led_pin, OUTPUT);
   digitalWrite(led_pin, LOW);
-  digitalWrite(power_pin, HIGH);
-  digitalWrite(motor_relay_pin, HIGH);
-  analogWrite(esc_pin, 0);
+  pinMode(brake_pin, INPUT);
+  // Timer0 stuff {
+  TCCR0A = 0;// set entire TCCR0A register to 0
+  TCNT0  = 0;//initialize counter value to 0
+  TCCR0B = 0;// same for TCCR0B
+  OCR0A = 7;// = (16*10^6) / (2000*64) - 1 (must be <256)
+  // 1024 prescaler:
+  TCCR0B |= (1 << CS02) | (0 << CS01) | (1 << CS00); // see page 87 of atmega328p datasheet
+  // http://ww1.microchip.com/downloads/en/DeviceDoc/Atmel-7810-Automotive-Microcontrollers-ATmega328P_Datasheet.pdf
+  // this mode sets the 1024 bit prescaler
+  TIMSK0 |= (1 << OCIE0A); // page 88, timer/counter interrupt mask register
+  //  "The corresponding interrupt is executed if a compare match in Timer/Counter0 occurs"
+  // } timer stuff
+
+
   delay(1000);
-  // analogWrite(esc_pin, 90);
-  // If digitalPinToInterrupt breaks, here is the mapping:
-  // p->i: 3->0
-
-  // attachInterrupt(digitalPinToInterrupt(speed_pin), update_speed, FALLING);
-  //attachInterrupt(digitalPinToInterrupt(power_pin), power_motor, RISING);
-
-  //attachInterrupt(digitalPinToInterrupt(power_pin), stop_motor, FALLING);
-  //attachInterrupt(4, test_rise, RISING);
-  //attachInterrupt(4, test_fall, FALLING);
-  attachInterrupt(digitalPinToInterrupt(4), test_change, CHANGE);
 
   Serial.begin(115200);
   Serial.println("hi");
   last_time = 0;
-  speed = 0;
   motor_state = false;
   show_speed = false;
   motor_speed = 0;
   display_speed();
   Serial.println("finished setup");
+  sei();//allow interrupts
 }
 
-long position  = -999;
+ISR(TIMER0_COMPA_vect){//timer0 interrupt 2kHz reads encoder + brake
+  long newPos = myEnc.read();
+  if (newPos != position) {
+    position = newPos;
+    // Serial.println(position);
+  }
+   // if brake pin is high, REPORT that
+   brake = digitalRead(brake_pin); // slow
+}
+
 
 void loop()
 {
-
-    long newPos = myEnc.read();
-    if (newPos != position) {
-      position = newPos;
-    //  Serial.println(position); // need to map this to motor speed
-    }
-
   if (has_display) {
     unsigned long cur_time = millis();
     if (cur_time - prev_show_time >= 700) {
       show_speed = true;
     }
   }
+  // brake = false; // #DEBUG
+  // see if brake is on
+  if(brake==false){
+    power_motor();
+  }
+  else{
+    stop_motor();
+  }
+  lightDance();
 }
--- README.md
+++ README.md
@@ -2,9 +2,25 @@
 
 lowkey mad about having to design a scooter firmware which is why id much rather rip off someone else's design
 
-* stole some stuff from this guy too
-https://github.com/Michaelhobo/electric-scooter
-
 ## status
-* hooked up an encoder which seems to be working OK
-* need to figure out PWM for the motor, this isn't hard but i still gotta do it
+* hooked up an encoder which seems to be working OK -> encoder now works with 2kHz interrupt on timer0
+* motor PWM is analogWrite, who cares about timing  
+
+## todo -> electrics
+* hook up a relay to determine if we're tryna brake lol
+* dead man's switch for throttle?
+* LEDs!!
+* brake light w/interrupt
+
+## citations
+random code i borrowed or stole or whatevever
+* https://www.instructables.com/id/Arduino-Timer-Interrupts/
+* https://github.com/Michaelhobo/electric-scooter
+
+
+## hardware stuff
+* scooter: using some weird old sharper image scooter. (As u may know, Sharper image won several international awards for their pioneering work in electric scooter design.) i dont know the exact specs and there's no documentation anyway so
+* motor: something w/ 24V, wattage unkown
+* rotary encoder: H38S400B, https://www.aliexpress.com/item/1000005665954.html -> i think it's around 400 pulses / revolution
+* arduino thing: don't get me started on this it's old Makerbot extruder controller 3.6 but since they went closed source they took all the docs offline so i've just had to take what i can get, it's based off the arduino diecimila
+* BMS: idk dude
